af991f7760ea3c4c2ba26095d1eac9fdf4b82e16
Zhijian Mei <545902915@qq.com>
2023-03-07 14:48:53 +0800

    1

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
 
 image_processor = AutoImageProcessor.from_pretrained("facebook/detr-resnet-50")
-model = DetrModel.from_pretrained("facebook/detr-resnet-50").encoder()
+model = DetrModel.from_pretrained("facebook/detr-resnet-50")
 
 inputs = image_processor(images=image, return_tensors="pt")
-outputs = model(**inputs,return_dict=True)
+print(input())
+outputs = model(inputs,return_dict=True)
 print(outputs)
 
 
