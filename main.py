f7a631eb5f65ddb5a63081fe1a05e8d094c5cb01
Zhijian Mei <545902915@qq.com>
2023-03-07 20:19:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -39,19 +39,15 @@ for filename in os.listdir(in_directory):
     if len(in_img_buffer) == 5:
         inputs = image_processor(images=in_img_buffer, return_tensors="pt").to(device)
         outputs = model(**inputs).encoder_last_hidden_state.detach()
-        print(outputs.shape)
         for i in range(outputs.shape[0]):
             if in_img_id_butter[i] not in out_img_features:
                 out_img_features[in_img_id_butter[i]] = outputs[i,:,:].cpu().numpy()
             else:
                 print('error')
                 quit()
-            print(out_img_features)
-        print(out_img_features)
-        np.save('imgs_features.npy',out_img_features)
-        datas = np.load('imgs_features.npy',allow_pickle=True)
-        print(datas)
-        quit()
+
+np.save('imgs_features.npy',out_img_features)
+
 
 
 
