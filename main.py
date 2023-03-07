cf47dfe6246ee388ffe45bdd4045c9dbb1ae29db
Zhijian Mei <545902915@qq.com>
2023-03-07 15:14:38 +0800

    1

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from transformers import DetrImageProcessor, DetrForObjectDetection, DetrModel
 import torch
-from PIL import Image
-import requests
+
 
 device=torch.device('cuda:2')
 print(device)
@@ -13,11 +11,12 @@ import requests
 url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
 
-image_processor = AutoImageProcessor.from_pretrained("facebook/detr-resnet-50",device=device)
+image_processor = AutoImageProcessor.from_pretrained("facebook/detr-resnet-50")
 model = DetrModel.from_pretrained("facebook/detr-resnet-50")
 
 # prepare image for the model
 inputs = image_processor(images=image, return_tensors="pt")
-print(inputs)
+outputs = model(**inputs)
+print(outputs)
 
 
