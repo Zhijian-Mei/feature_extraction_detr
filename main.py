4788cf72d8c65dfc5a8ee08e65db5d39826f2a58
Zhijian Mei <545902915@qq.com>
2023-03-07 19:41:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,25 +8,29 @@ from transformers import AutoImageProcessor, DetrModel
 from PIL import Image
 import requests
 import os
+import numpy as np
 
 in_directory = 'amazon_imgs'
 out_directory = 'amazon_imgs_feature'
 
+image_processor = AutoImageProcessor.from_pretrained("facebook/detr-resnet-50")
+model = DetrModel.from_pretrained("facebook/detr-resnet-50").to(device)
+
+in_img_buffer = []
 for filename in os.listdir(in_directory):
     f = os.path.join(in_directory, filename)
     # checking if it is a file
     if os.path.isfile(f):
         id = filename[:len(filename)-4]
-        print(filename)
-        print(id)
+        image = Image.open(f)
+        print(image)
         quit()
+        in_img_buffer.append(image)
 
+    if len(in_img_buffer) == 10:
+        images = torch.FloatTensor(in_img_buffer)
+        inputs = image_processor(images=images, return_tensors="pt").to(device)
 
-url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-image = Image.open()
-
-image_processor = AutoImageProcessor.from_pretrained("facebook/detr-resnet-50")
-model = DetrModel.from_pretrained("facebook/detr-resnet-50").to(device)
 
 # prepare image for the model
 inputs = image_processor(images=image, return_tensors="pt").to(device)
