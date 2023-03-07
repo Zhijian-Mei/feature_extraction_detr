c4408f864aef2a9e323ca5e2041e4d90fc0d5099
Zhijian Mei <545902915@qq.com>
2023-03-07 19:29:07 +0800

    1

--- main.py
+++ main.py
@@ -7,9 +7,21 @@ print(device)
 from transformers import AutoImageProcessor, DetrModel
 from PIL import Image
 import requests
+import os
+
+in_directory = 'amazon_imgs'
+out_directory = 'amazon_imgs_feature'
+
+for filename in os.listdir(in_directory):
+    f = os.path.join(in_directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        print(filename)
+        quit()
+
 
 url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
+image = Image.open()
 
 image_processor = AutoImageProcessor.from_pretrained("facebook/detr-resnet-50")
 model = DetrModel.from_pretrained("facebook/detr-resnet-50").to(device)
@@ -18,6 +30,6 @@ model = DetrModel.from_pretrained("facebook/detr-resnet-50").to(device)
 inputs = image_processor(images=image, return_tensors="pt").to(device)
 outputs = model(**inputs)
 print(outputs.encoder_last_hidden_state)
-print(outputs.encoder_last_hidden_state.shape)
+
 
 
