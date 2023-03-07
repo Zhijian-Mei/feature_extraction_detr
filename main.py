155bff7fd95715bc8a209411a55b477a4155a7f0
Zhijian Mei <545902915@qq.com>
2023-03-07 19:43:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,17 @@ import requests
 import os
 import numpy as np
 
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+print(image)
+quit()
+
 in_directory = 'amazon_imgs'
 out_directory = 'amazon_imgs_feature'
 
 image_processor = AutoImageProcessor.from_pretrained("facebook/detr-resnet-50")
 model = DetrModel.from_pretrained("facebook/detr-resnet-50").to(device)
+
 
 in_img_buffer = []
 for filename in os.listdir(in_directory):
