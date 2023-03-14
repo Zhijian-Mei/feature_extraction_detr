2bc073edc2910c365b2f23bbf0d8f82b2e871cf7
Zhijian Mei <545902915@qq.com>
2023-03-14 16:01:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,8 @@ for filename in os.listdir(in_directory):
             inputs = image_processor(images=image, return_tensors="pt").to(device)
         except:
             continue
-        outputs = model(**inputs).encoder_last_hidden_state.detach()
+        # outputs = model(**inputs).encoder_last_hidden_state.detach()
+        outputs = model(**inputs).decoder_last_hidden_state.detach()
         if id not in out_img_features:
             out_img_features[id] = outputs.cpu().numpy()
             print(outputs.shape)
